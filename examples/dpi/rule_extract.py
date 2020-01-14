87edc9b4bae5f170829fbf9c0bd7f5509129bc68
yangzhou <yangzhou@node-0.yangzhou-qv63200.lambda-mpi-pg0.clemson.cloudlab.us>
2020-01-14 01:12:40 -0500

    update rules

--- examples/dpi/rule_extract.py
+++ examples/dpi/rule_extract.py
@@ -85,12 +85,43 @@ if __name__ == "__main__":
 
     f_sgx = open("./rules/dpirules.rs", "w")
     cnt = 0
-    for i in word_ruleset:
+    for i in sente_ruleset:
         if cnt == 0:
-            f_sgx.write("pub const DPIRULES: [&str; %d] = [r#\"" % (len(word_ruleset),) + i + "\"#, ")
-        elif cnt == len(word_ruleset) - 1:
+            f_sgx.write("pub const DPIRULES: [&str; %d] = [r#\"" % (len(sente_ruleset),) + i + "\"#, ")
+        elif cnt == len(sente_ruleset) - 1:
             f_sgx.write("r#\"" + i + "\"#];")
         else:
             f_sgx.write("r#\"" + i + "\"#, ")
         cnt += 1
     f_sgx.close()
+
+    f_hs = open("./rules/hs.rules", "w")
+    lite_sente_ruleset = []
+    for i, rule in enumerate(sente_ruleset):
+        new_rule = ''
+        for c in rule:
+            if c in ['[', ']', '(', ')', '{', '}', '-', '*', '+', '\\', '|', '/', ':', '^', '.', '$', '?']:
+                new_rule += '\\' + c
+            else:
+                new_rule += c
+            import re
+            # try:
+            #     re.compile(new_rule)
+            # except:
+            #     print(new_rule)
+            lite_sente_ruleset.append(new_rule)
+        f_hs.write(f'{i}:/{new_rule}/\n')
+    f_hs.close()
+
+
+    f_hs_sgx = open("./rules/dpihsrules.rs", "w")
+    cnt = 0
+    for i in lite_sente_ruleset:
+        if cnt == 0:
+            f_hs_sgx.write("pub const HSDPIRULES: [&str; %d] = [r#\"" % (len(lite_sente_ruleset),) + i + "\"#, ")
+        elif cnt == len(lite_sente_ruleset) - 1:
+            f_hs_sgx.write("r#\"" + i + "\"#];")
+        else:
+            f_hs_sgx.write("r#\"" + i + "\"#, ")
+        cnt += 1
+    f_hs_sgx.close()
