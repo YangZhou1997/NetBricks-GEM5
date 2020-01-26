ad51d022be56beb42ad0eb37d1f537e8dbef94fd
yangzhou <yangzhou@node-0.yangzhou-qv63200.lambda-mpi-pg0.clemson.cloudlab.us>
2020-01-26 12:59:09 -0500

    switch to 1M pkts

--- framework/src/scheduler/mod.rs
+++ framework/src/scheduler/mod.rs
@@ -15,7 +15,7 @@ pub mod embedded_scheduler;
 mod standalone_scheduler;
 
 // pub const PKT_NUM: u64 = (100 * 1024);
-pub const PKT_NUM: u64 = (2 * 1024 * 2014);
+pub const PKT_NUM: u64 = (1 * 1024 * 2014);
 
 /// Errors related to schedulers/scheduling
 // TODO: extend this, as we probably want more error handling over
